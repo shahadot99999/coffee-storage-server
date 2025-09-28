2fdda829d40183b026ea59bb2da9b7e118c19547
shahadot99999 <shahadot99999@gmail.com>
2025-09-28 17:40:14 +0600

    server side get data using

--- index.js
+++ index.js
@@ -85,6 +85,11 @@ async function run() {
 
 
     //user related apis
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const userProfile = req.body;
       console.log(userProfile);
